269a25f52f0ce109f374d7903870a01af719eb8c
quimaruanomed <jruanomed@gmail.com>
2020-10-29 18:51:28 +0100

    first commit

---
+++ Fase1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fase1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Joaquina";
+            string firstname = "Ruano";
+            string surname = "Medina";
+            int day = 29, month = 10, any = 2020; 
+
+            Console.WriteLine(firstname + " "+ surname + " " + name  );
+            Console.WriteLine(day + "/" + month + "/" + "/"+any);
+
+        }
+    }
+}
